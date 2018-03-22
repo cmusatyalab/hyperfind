f6b82c1000c30c03c85145adb9ff45ab9f92b8b5
Jan Harkes <jaharkes@cs.cmu.edu>
2018-03-22 09:44:45 -0400

    Improvements for the get-libs script
    We don't really need bash, any bourne shell should do.
    Make sure we're running from the right directory.
    Avoid error messages when find fails to find .jar files.

--- lib/get-lib.sh
+++ lib/get-lib.sh
@@ -1,7 +1,8 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
+cd "$(dirname "$0")" || exit
 
-find jai_imageio.jar -type f || wget -nv https://github.com/jai-imageio/jai-imageio-core/releases/download/jai-imageio-core-1.3.1/jai-imageio-core-1.3.1.jar -O jai_imageio.jar
-find swingx.jar -type f || wget -nv http://repo1.maven.org/maven2/org/swinglabs/swingx/swingx-all/1.6.4/swingx-all-1.6.4.jar -O swingx.jar
+[ -e jai_imageio.jar ] || wget -nv https://github.com/jai-imageio/jai-imageio-core/releases/download/jai-imageio-core-1.3.1/jai-imageio-core-1.3.1.jar -O jai_imageio.jar
+[ -e swingx.jar ] || wget -nv http://repo1.maven.org/maven2/org/swinglabs/swingx/swingx-all/1.6.4/swingx-all-1.6.4.jar -O swingx.jar
 
 md5sum -c MD5SUMS
