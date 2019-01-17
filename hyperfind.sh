d75abcb67af077cd2f26e13ba8100ec251910eba
Ziqiang Feng <zf@cs.cmu.edu>
2019-01-17 14:17:25 -0500

    allow changing bundle and filter dirs in launch script

--- hyperfind.sh
+++ hyperfind.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-CLASSPATH="hyperfind.jar:/usr/share/java/opendiamond.jar:lib/swingx.jar:lib/jai_imageio.jar:lib/gson.jar:lib/commons-io-2.6/*"
-BUNDLES="$HOME/.diamond/codecs:$HOME/.diamond/predicates:/usr/local/share/diamond/codecs:/usr/local/share/diamond/predicates"
-FILTERS="$HOME/.diamond/filters:/usr/local/share/diamond/filters"
-java -cp $CLASSPATH edu.cmu.cs.diamond.hyperfind.Main $BUNDLES $FILTERS
-
+echo using bundle dirs: ${BUNDLES:="$HOME/.diamond/codecs:$HOME/.diamond/predicates:/usr/local/share/diamond/codecs:/usr/local/share/diamond/predicates"}
+echo using filters dir: ${FILTERS:="$HOME/.diamond/filters:/usr/local/share/diamond/filters"}
+java -jar $(dirname "$0")/hyperfind.jar $BUNDLES $FILTERS
