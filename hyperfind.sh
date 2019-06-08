e2cd619a49b8f124c690a22b0ca9e75535ec0488
Ziqiang Feng <zf@cs.cmu.edu>
2019-06-08 15:47:11 -0400

    Add debian packaging control files

--- hyperfind.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-echo using bundle dirs: ${BUNDLES:="$HOME/.diamond/codecs:$HOME/.diamond/predicates:/usr/local/share/diamond/codecs:/usr/local/share/diamond/predicates"}
-echo using filters dir: ${FILTERS:="$HOME/.diamond/filters:/usr/local/share/diamond/filters"}
-java -jar $(dirname "$0")/hyperfind.jar $BUNDLES $FILTERS
