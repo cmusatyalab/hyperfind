db8a451f76f36c74c3e8ab162ee35569f2f54904
wangkaiwen998@gmail.com <wangkaiwen998@gmail.com>
2019-12-08 12:14:41 -0500

    added pwd arg

--- hyperboard/app/__init__.py
+++ hyperboard/app/__init__.py
@@ -2,23 +2,28 @@ from flask import Flask
 
 app = Flask(__name__)
 
-import sys, os
-logdir_key = "HYPERBOARD_LOGDIR"
-if logdir_key in os.environ:
-    app.config["ROOT_FOLDER"] = os.environ[logdir_key]
-    if app.config["ROOT_FOLDER"] == '':
-        print("require logdir as an argument!")
-        os._exit(-1)
+def print_usage():
+    print("usage: ./hyperboard logdir pwd")
 
-    # make absolute path
-    if not os.path.isabs(app.config["ROOT_FOLDER"]):
-        # the ../ is to take into account the hyperboard folder, since we
-        # assume the caller is using hyperfind's script
-        app.config["ROOT_FOLDER"] = os.path.abspath("../" + app.config["ROOT_FOLDER"])
-    print("Hyperboard using as root folder ", app.config["ROOT_FOLDER"])
-else:
-    print("require a valid root folder!")
+import sys, os
+pwd_key = "HYPERBOARD_PWD"
+logdir_key = "HYPERBOARD_LOGDIR"
+assert(logdir_key in os.environ)
+assert(pwd_key in os.environ)
+pwd = os.environ[pwd_key]
+logdir = os.environ[logdir_key]
+if pwd == '' or logdir == '':
+    print("Input pwd = %s, logdir = %s" % (pwd, logdir))
+    print_usage()
     os._exit(-1)
+
+# make logdir absolute path by using pwd
+assert(os.path.isabs(pwd))
+if not os.path.isabs(logdir):
+    logdir = os.path.join(pwd, logdir)
+app.config["ROOT_FOLDER"] = logdir
+
+print("Hyperboard using as root folder ", app.config["ROOT_FOLDER"])
 
 from app import routes
 from app import logs
